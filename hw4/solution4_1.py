783e7b6d9ba4853247e40790e93f232ff009dd37
Denis Porsev <den.porsev@gmail.com>
2021-11-30 12:35:19 +0300

    Code style related changes

--- hw4/solution4_1.py
+++ hw4/solution4_1.py
@@ -25,26 +25,16 @@ def qf_trap(f, a, b):
 
 # АСТ 3
 def qf_simp(f, a, b):
-    return (b - a) * (
-        f(a) +
-        4 * f((a + b) / 2) +
-        f(b)
-    ) / 6
+    return (b - a) * (f(a) + 4 * f((a + b) / 2) + f(b)) / 6
 
 
 # АСТ 3
 def qf_3_8(f, a, b):
     h = (b - a) / 3
-    return (b - a) * (
-        f(a) +
-        3 * f(a + h) +
-        3 * f(a + 2 * h) +
-        f(b)
-    ) / 8
+    return (b - a) * (f(a) + 3 * f(a + h) + 3 * f(a + 2 * h) + f(b)) / 8
 
 
 def main_loop():
-
     def quad_f(f, a, b):
         integral, _ = integrate.quad(f, a, b)
         return integral
@@ -55,7 +45,7 @@ def main_loop():
         (lambda x: x, "x"),
         (lambda x: x ** 2, "x^2"),
         (lambda x: x ** 3, "x^3"),
-        (lambda x: math.e ** ((-x)**2), "e^(-x^2)")
+        (lambda x: math.e ** ((-x) ** 2), "e^(-x^2)"),
     ]
 
     quadr_formulas = [
@@ -64,7 +54,7 @@ def main_loop():
         qf_inter_rect,
         qf_trap,
         qf_simp,
-        qf_3_8
+        qf_3_8,
     ]
 
     while True:
@@ -78,12 +68,18 @@ def main_loop():
 
             qf_names = [qf.__name__ for qf in quadr_formulas]
 
-            print(f'\nf(x) = {f_name}')
-            print(f'Exact integral: {corr_integral}\n')
-            print(tabulate([(qf_name, y, err) for qf_name, y, err in
-                            zip(qf_names, appr_integrals, errors)],
-                           headers=["quad_formula", "appr", "err"],
-                           floatfmt=".10f"))
+            print(f"\nf(x) = {f_name}")
+            print(f"Exact integral: {corr_integral}\n")
+            print(
+                tabulate(
+                    [
+                        (qf_name, y, err)
+                        for qf_name, y, err in zip(qf_names, appr_integrals, errors)
+                    ],
+                    headers=["quad_formula", "appr", "err"],
+                    floatfmt=".10f",
+                )
+            )
             print()
 
         if input("\nПродолжить с новыми a, b? (y, n): ") == "y":
@@ -92,7 +88,9 @@ def main_loop():
             break
 
 
-if __name__ == '__main__':
-    print(f'''Задание 4. Приближенное вычисление интеграла по квадратурным формулам
----------------------------------------------------------------------''')
+if __name__ == "__main__":
+    print(
+        f"""Задание 4. Приближенное вычисление интеграла по квадратурным формулам
+---------------------------------------------------------------------"""
+    )
     main_loop()
