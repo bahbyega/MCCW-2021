a69c5e28d252967ae949f566cce2f3ad3c47aa30
Denis Porsev <den.porsev@gmail.com>
2021-12-07 00:54:10 +0300

    Refactored the code to make it look decent

--- hw5/solution5.py
+++ hw5/solution5.py
@@ -1,72 +1,92 @@
-import math
+"""
+This module contains solutions for fifth laboratory task of
+Methods of Computation and Computational Workshop course.
+"""
 
-from typing import Final, Sequence, Callable, Tuple
-from scipy.special import legendre
+import math
+from typing import Callable, Final, Sequence, Tuple
 
 from hw1.solution1 import root_finding, secant
 
-SEG_DEFAULT_BEG: Final = -1
-SEG_DEFAULT_END: Final = 1
 
-
-def _legendre(x, n) -> float:
-    if n == 0:
+def _legendre(x_val, num_nodes) -> float:
+    """
+    Calculates legendre function using recurrent formula for homogeneous polynomials
+    """
+    if num_nodes == 0:
         return 1
-    if n == 1:
-        return x
-    return (2 * n - 1) / n * _legendre(x, n - 1) * x - (n - 1) / n * _legendre(x, n - 2)
+    if num_nodes == 1:
+        return x_val
+
+    return (2 * num_nodes - 1) / num_nodes * _legendre(x_val, num_nodes - 1) * x_val - (
+        num_nodes - 1
+    ) / num_nodes * _legendre(x_val, num_nodes - 2)
 
 
-def print_root_coef_pares(nodes_list: Sequence[int]) -> None:
-    epsilon = 10 ** (-21)
+def print_node_coef_pares(pares: Tuple[float, float]) -> float:
+    """
+    Prints the pares of nodes and values in "node <-> value" format.
+    Returns checksum of coefficients.
+    """
+    checksum = 0.0
 
-    for node in nodes_list:
-        print(f"\nN = {node}")
+    for node, coef in pares:
+        print(f"{node:.12f} <-> {coef:.12f}")
+        checksum += coef
 
-        root_coef_pares = find_gauss_integral(node, _legendre, epsilon)
+    return checksum
 
-        for root, coef in root_coef_pares:
-            print(f"{root:.12f} <-> {coef:.12f}")
+
+def compute_gauss_node_coef_pares(num_nodes: int) -> Sequence[Tuple[float, float]]:
+    """
+    Calculate pares of nodes and coefficients with gauss' quadratic formula
+    """
+    node_coef_pares = []
+
+    segments = root_finding(
+        lambda x: _legendre(x, num_nodes), SEG_DEFAULT_BEG, SEG_DEFAULT_END
+    )
+
+    for seg in segments:
+        node = secant(
+            lambda x: _legendre(x, num_nodes), seg[0], seg[1], EPS_DEFAULT_VAL
+        )
+        coef = (2 * (1 - node ** 2)) / (
+            num_nodes ** 2 * _legendre(node, num_nodes - 1) ** 2
+        )
+
+        node_coef_pares.append((node, coef))
+
+    return node_coef_pares
+
+
+def compute_meler_node_coef_pares(num_nodes: int) -> Sequence[Tuple[float, float]]:
+    """
+    Calculate pares of nodes and coefficients with meler's quadratic formula
+    """
+    node_coef_pares = []
+
+    coef = math.pi / num_nodes
+    for num in range(1, num_nodes + 1):
+        node = math.cos((2 * num - 1) * math.pi / (2 * num_nodes))
+        node_coef_pares.append((node, coef))
+
+    return node_coef_pares
 
 
 def find_gauss_integral(
-    node: int, func: Callable, epsilon: float = 10 ** (-12)
-) -> Sequence[Tuple[float, float]]:
-    root_coef_pares = []
-
-    segments = root_finding(lambda x: func(x, node), SEG_DEFAULT_BEG, SEG_DEFAULT_END)
-
-    for seg in segments:
-        root = secant(lambda x: func(x, node), seg[0], seg[1], epsilon)
-        coef = (2 * (1 - root ** 2)) / (node ** 2 * func(root, node - 1) ** 2)
-
-        root_coef_pares.append((root, coef))
-
-    return root_coef_pares
-
-
-def find_meler_integral(node: int, func: Callable) -> float:
-    integral_sum = 0
-
-    for k in range(1, node + 1):
-        root = math.cos((2 * k - 1) * math.pi / (2 * node))
-        print(f"{root} <-> {math.pi / node}")
-        integral_sum += func(root)
-
-    return math.pi * integral_sum / node
-
-
-def calc_integral_sum_from_roots_and_coeffs(
-    root_coef_pares: Tuple[float, float], func: Callable, seg_a: float, seg_b: float
+    node_coef_pares: Tuple[float, float], func: Callable, seg_a: float, seg_b: float
 ) -> float:
     """
-    Linearly maps root_coef_paresn on [seg_a, seg_b] to [-1, 1],
-    Calculates mapped integral sum
+    Linearly maps node_coef_paresn on [seg_a, seg_b] to [-1, 1],
+
+    Given the pares of nodes and coefficient finds the approximate value of
+    an integral on [-1, 1] of function in func.
     """
     similarity_coefficient = (seg_b - seg_a) / (SEG_DEFAULT_END - SEG_DEFAULT_BEG)
 
     mapped_integral_sum = 0
-    for root, coef in root_coef_pares:
+    for root, coef in node_coef_pares:
         mapped_coef = coef * similarity_coefficient
         mapped_root = seg_a + similarity_coefficient * (root - SEG_DEFAULT_BEG)
         mapped_integral_sum += mapped_coef * func(mapped_root)
@@ -74,46 +94,142 @@ def calc_integral_sum_from_roots_and_coeffs(
     return mapped_integral_sum
 
 
+def find_meler_integral(node_coef_pares: Tuple[float, float], func: Callable) -> float:
+    """
+    Given the pares of nodes and coefficient finds the approximate value of
+    an integral on [-1, 1] of function in func.
+    """
+    integral_sum = 0
+    num_nodes = len(node_coef_pares)
+
+    for i in range(num_nodes):
+        node = node_coef_pares[i][0]
+        integral_sum += func(node)
+
+    return math.pi * integral_sum / num_nodes
+
+
 def do_accuracy_check_for(nodes_list: Sequence[int]) -> None:
-    for node in nodes_list:
-        if node == 3:
-            f = lambda x: 6 * x ** 5 + 2 * x + 34
+    """
+    Basically a test (or either 3 tests) of compute compute_gauss_node_coef_pares()
+    on three different numbers of nodes for interpolated quadratic formula
+    """
+    for num_nodes in nodes_list:
+        if num_nodes == 3:
+            func = lambda x: 6 * x ** 5 + 2 * x + 34
 
             exact_integral = 68
-            root_coef_pares = find_gauss_integral(node, _legendre)
-            approx_integral = calc_integral_sum_from_roots_and_coeffs(
-                root_coef_pares, f, SEG_DEFAULT_BEG, SEG_DEFAULT_END
+            node_coef_pares = compute_gauss_node_coef_pares(num_nodes)
+            approx_integral = find_gauss_integral(
+                node_coef_pares, func, SEG_DEFAULT_BEG, SEG_DEFAULT_END
             )
 
             print(f"\nPolynom: 6x^5 + 2x + 34, exact integral = {exact_integral}")
             print(f"Approximate integral: {approx_integral:.12f}")
 
-        elif node == 4:
-            f = lambda x: 8 * x ** 7 + 3 * x ** 2
+        elif num_nodes == 4:
+            func = lambda x: 8 * x ** 7 + 3 * x ** 2
 
             exact_integral = 2
-            root_coef_pares = find_gauss_integral(node, _legendre)
-            approx_integral = calc_integral_sum_from_roots_and_coeffs(
-                root_coef_pares, f, SEG_DEFAULT_BEG, SEG_DEFAULT_END
+            node_coef_pares = compute_gauss_node_coef_pares(num_nodes)
+            approx_integral = find_gauss_integral(
+                node_coef_pares, func, SEG_DEFAULT_BEG, SEG_DEFAULT_END
             )
 
             print(f"\nPolynom: 8x^7 + 3x^2, exact integral = {exact_integral}")
             print(f"Approximate integral: {approx_integral:.12f}")
 
         else:
-            f = lambda x: 10 * x ** 9 + 5 * x ** 4 + 1
+            func = lambda x: 10 * x ** 9 + 5 * x ** 4 + 1
 
             exact_integral = 4
-            root_coef_pares = find_gauss_integral(node, _legendre)
-            approx_integral = calc_integral_sum_from_roots_and_coeffs(
-                root_coef_pares, f, SEG_DEFAULT_BEG, SEG_DEFAULT_END
+            node_coef_pares = compute_gauss_node_coef_pares(num_nodes)
+            approx_integral = find_gauss_integral(
+                node_coef_pares, func, SEG_DEFAULT_BEG, SEG_DEFAULT_END
             )
 
             print(f"\nPolynom: 10x^9 + 5x^4 + 1, exact integral = {exact_integral}")
             print(f"Approximate integral: {approx_integral:.12f}")
 
 
+def do_task_1() -> None:
+    """
+    Runs task 1 output
+    """
+    print("\n-------------------------------\nЗадание 1.")
+    num_nodes_list = list(range(1, 9))
+
+    for num_nodes in num_nodes_list:
+        node_coef_pares = compute_gauss_node_coef_pares(num_nodes)
+        print(f"\nЧисло узлов: {num_nodes}")
+        print(f"Checksum: {print_node_coef_pares(node_coef_pares):.12f}")
+
+
+def do_task_2() -> None:
+    """
+    Runs task 2 output
+    """
+    print("\n-------------------------------\nЗадание 2.")
+
+    num_nodes_list = [3, 4, 5]
+    print(f"Узлы: {num_nodes_list}")
+    do_accuracy_check_for(num_nodes_list)
+
+
+def do_task_3() -> None:
+    """
+    Runs task 3 output
+    """
+    print("\n-------------------------------\nЗадание 3.")
+
+    num_nodes_list = list(
+        map(int, input("Введите список числа узлов (до 4 узлов): ").strip().split())
+    )[:4]
+
+    seg_a = input("Введите a (default = 0):")
+    seg_b = input("Введите b (default = 1):")
+
+    seg_a = EXAMPLE_GAUSS_SEG_BEG if seg_a == "" else float(seg_a)
+    seg_b = EXAMPLE_GAUSS_SEG_END if seg_b == "" else float(seg_b)
+
+    for num_nodes in num_nodes_list:
+        node_coef_pares = compute_gauss_node_coef_pares(num_nodes)
+
+        approx_integral = find_gauss_integral(
+            node_coef_pares,
+            example_gauss,
+            seg_a,
+            seg_b,
+        )
+
+        print(f"\nN = {num_nodes}")
+        print(f"Checksum: {print_node_coef_pares(node_coef_pares):.12f}")
+        print(f"\nExact integral = {EXAMPLE_GAUSS_EXACT_INTEGRAL}")
+        print(f"Approximate integral = {approx_integral}")
+        print(f"Error: {abs(approx_integral - EXAMPLE_GAUSS_EXACT_INTEGRAL)}")
+
+
+def do_task_4() -> None:
+    """
+    Runs task 4 output
+    """
+    print("\n-------------------------------\nЗадание 4.")
+
+    num_nodes_list = list(
+        map(int, input("Введите список числа узлов (до 3 узлов): ").strip().split())
+    )[:3]
+
+    for num_nodes in num_nodes_list:
+        print(f"\nN = {num_nodes}")
+        node_coef_pares = compute_meler_node_coef_pares(num_nodes)
+        approx_integral = find_meler_integral(node_coef_pares, example_meler)
+
+        print(f"Checksum: {print_node_coef_pares(node_coef_pares):.12f}")
+        print(f"\nApproximate integral = {approx_integral}")
+
+
 if __name__ == "__main__":
+
     print(
         """
     Задание 5. КФ Гаусса, ее узлы и коэффициенты.
@@ -124,66 +240,27 @@ if __name__ == "__main__":
     """
     )
 
+    SEG_DEFAULT_BEG: Final = -1
+    SEG_DEFAULT_END: Final = 1
+    EPS_DEFAULT_VAL: Final = 10 ** (-20)
+
     # Variant 6
     example_gauss = lambda x: x * math.log(1 + x)
-    example_gauss_seg_beg = 0
-    example_gauss_seg_end = 1
-    example_gauss_exact_integral = 0.25
-
-    epsilon = 10 ** (-12)
-
     example_meler = lambda x: math.cos(x) ** 2
 
+    EXAMPLE_GAUSS_SEG_BEG = 0
+    EXAMPLE_GAUSS_SEG_END = 1
+    EXAMPLE_GAUSS_EXACT_INTEGRAL = 0.25
+
     while True:
-        print("\n-------------------------------\nЗадание 1.")
-        print_root_coef_pares(list(range(1, 9)))
+        do_task_1()
 
-        print("\n-------------------------------\nЗадание 2.")
-        nodes_list = [3, 4, 5]
-        print(f"Узлы: {nodes_list}")
-        do_accuracy_check_for(nodes_list)
+        do_task_2()
 
-        print("\n-------------------------------\nЗадание 3.")
+        do_task_3()
 
-        nodes_list = list(
-            map(int, input("Введите список числа узлов (до 4 узлов): ").strip().split())
-        )[:4]
+        do_task_4()
 
-        a = input("Введите a (default = 0):")
-        b = input("Введите b (default = 1):")
-
-        a = example_gauss_seg_beg if a == "" else float(a)
-        b = example_gauss_seg_end if b == "" else float(b)
-
-        for node in nodes_list:
-            root_coef_pares = find_gauss_integral(node, _legendre, epsilon)
-
-            approx_integral = calc_integral_sum_from_roots_and_coeffs(
-                root_coef_pares,
-                example_gauss,
-                a,
-                b,
-            )
-
-            print(f"\nN = {node}")
-            for root, coef in root_coef_pares:
-                print(f"{root:.12f} <-> {coef:.12f}")
-
-            print(f"\nExact integral = {example_gauss_exact_integral}")
-            print(f"Approximate integral = {approx_integral}")
-            print(f"Error: {abs(approx_integral - example_gauss_exact_integral)}")
-
-        print("\n-------------------------------\nЗадание 4.")
-
-        nodes_list = list(
-            map(int, input("Введите список числа узлов (до 3 узлов): ").strip().split())
-        )[:3]
-
-        for node in nodes_list:
-            print(f"\nN = {node}")
-            approx_integral = find_meler_integral(node, example_meler)
-            print(f"Approximate integral = {approx_integral}")
-
-        if input("\nПродолжить с новыми nodes, a, b? (y, n): ") == "y":
+        if input("\nПродолжить с новыми узлами, a, b? (y, n): ") == "y":
             continue
         break
